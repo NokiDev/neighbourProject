30d6220d759aadd9bad12d045bb326da942ea23b
bluedragonfly <noki.dev@gmail.com>
2016-03-18 15:14:21 +0100

    Improved mapping on profile

--- public/script/view.js
+++ public/script/view.js
@@ -3,44 +3,6 @@ $(document).ready(function(){
 		$('#lidrop').toggle();
 	})
 
-//OnOff
-
-
-// array contenant les positions des markers
-    var locations = [
-      ['Abidjdan', 5.345317, -4.024429],
-      ['Gagnoa', 6.133333, -5.933333],
-      ['Korogho', 9.416667, -5.616667]
-    ];
-
-
-// definit le type de map, le zoom et la position de depart de la carte
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 6,
-      center: new google.maps.LatLng(6.133333, -5.933333),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-
-// l infozindo est la fenetre qui apparait lorsqu'on clique sur un marker
-    var infowindow = new google.maps.InfoWindow();
-
-    var marker, i;
-
-    for (i = 0; i < locations.length; i++) { 
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
-      });
-
-// s execute lorsque l on clique sur un marker
-// c est un evenement definit par l api
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(locations[i][0]);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-    }
-})
+});
 
 
